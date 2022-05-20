b48c57ad5e01e17b99b93d430812634dd7bea666
Hantonny Korrea <hantonny.20@gmail.com>
2022-05-19 22:41:16 -0400

    :sparkles: - Chapter I: Webpack Dev Server

--- reactjs/01-github-explorer/webpack.config.js
+++ reactjs/01-github-explorer/webpack.config.js
@@ -13,6 +13,10 @@ module.exports = {
     resolve: {
         extensions: ['.js', '.jsx'],
     },
+    devServer: {
+        static: path.resolve(__dirname, 'public'),
+    }
+    ,
     plugins: [
         new HtmlWebpackPlugin({
             template: path.resolve(__dirname, 'public', 'index.html'),
