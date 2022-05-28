fae71e153a74accf86090f302c8fcf8e06ae7055
Hantonny Korrea <hantonny.20@gmail.com>
2022-05-28 19:41:39 -0400

    :sparkles: - Chapter I: Utilizando o useEffect

--- reactjs/01-github-explorer/src/components/RepositoryList.jsx
+++ reactjs/01-github-explorer/src/components/RepositoryList.jsx
@@ -1,6 +1,9 @@
+import { useState, useEffect } from 'react'
 import { RepositoryItem } from "./RepositoryItem"
 
 import '../styles/repositories.scss'
+
+//https://api.github.com/users/hantonny/repos
 
 const repository = {
     name: 'unform',
@@ -9,6 +12,14 @@ const repository = {
 }
 
 export function RepositoryList() {
+    const [repositories, setRepositories] = useState([])
+
+    useEffect(() => {
+        fetch('https://api.github.com/users/hantonny/repos')
+            .then(response => response.json())
+            .then(data => setRepositories(data))
+    }, [])
+
     return (
         <section className="repository-list">
             <h1>Lista de Repositórios</h1>
