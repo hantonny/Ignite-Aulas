77162b895e8c81c08f16197f55f5b5970d9d23c7
Hantonny Korrea <hantonny.20@gmail.com>
2022-06-03 18:46:16 -0400

    :sparkles: - Chapter I: Componentes com TypeScript

--- reactjs/01-github-explorer/src/components/RepositoryList.tsx
+++ reactjs/01-github-explorer/src/components/RepositoryList.tsx
@@ -5,26 +5,37 @@ import '../styles/repositories.scss'
 
 //https://api.github.com/users/hantonny/repos
 
+interface Repository {
+    name: string,
+    description: string,
+    html_url: string,
+}
+
+interface Users {
+    name: string,
+    blog: string,
+}
+
 export function RepositoryList() {
-    const [repositories, setRepositories] = useState([])
-    const [users, setUsers] = useState([])
+    const [repositories, setRepositories] = useState<Repository[]>([])
+    const [users, setUsers] = useState<Users>({name: '', blog: ''})
 
     useEffect(() => {
         fetch('https://api.github.com/users/hantonny/repos')
             .then(response => response.json())
             .then(data => setRepositories(data))
+          
     }, [])
 
     useEffect(() => {
         fetch('https://api.github.com/users/hantonny')
-            .then(response => response.json())
-            .then(data => setUsers(data))
+        .then(response => response.json())
+        .then(data => setUsers(data))
     }, [])
-
+    
     return (
         <section className="repository-list">
             <h1>Lista de Repositórios de <a href={users.blog} target="_blank">{users.name}</a></h1>
-
             <ul>
                 {repositories.map(repository => {
                     return <RepositoryItem key={repository.name} repository={repository} />
